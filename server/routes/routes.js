8b88c33e9a7cd8134a602fa063fd2628d07b38e3
Maximiliano Martinez <maxilucasmartinez18@gmail.com>
2024-11-28 17:39:09 -0300

    Add getUsers and getUser

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,8 +3,8 @@ import { createUser, getUsers, getUser } from "../controllers/userControllers.js
 
 const router = Router();
 
-// router.route('/').get(getUsers);
-// router.route('/:id').get(getUser);
+router.route('/').get(getUsers);
+router.route('/:id').get(getUser);
 router.post("/", createUser)
 
 export default router;
